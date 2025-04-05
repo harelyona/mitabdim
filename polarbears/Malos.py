138ba73b82b0c8b4b4faffba90348b683c4c7d92
harel <harel.yona@mail.huji.ac.il>
2025-04-05 21:57:47 +0300

    a

--- polarbears/Malos.py
+++ polarbears/Malos.py
@@ -5,15 +5,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 from typing import Tuple
 
+GRAPH_TITLE_SIZE = 20
+INTENSITY_LABEL = 'Intensity [V]'
+DEG_LABEL = "Angle [Deg]"
 ANGLE_UNCERTAINTY = 0.5
 DATA_COLOR = "blue"
-FIT_COLOR = "lightgreen"
-ERRORBARS_COLOR = "red"
+FIT_COLOR = "black"
+ERRORBARS_COLOR = "lightcoral"
 DATA_POINTs_SIZE = 5
 FIGURE_SIZE = (8, 6)
+AXIS_LABEL_SIZE = 20
 
 
-# matplotlib.use('Qt5Agg')
+matplotlib.use('TkAgg')
 
 
 def extract_averages_from_excel(folder_name: str) -> np.ndarray:
@@ -62,14 +66,14 @@ def plot_double_polarizers(angle_polarizer_list, averages_list, save=False):
         ms=DATA_POINTs_SIZE
     )
     plt.plot(x_values, I_values, color='black', label=rf'$I = {I0:.2e} \cos^2(\theta)$')
-    plt.xlabel(r'Angle [$^\circ$]')
-    plt.ylabel('Intensity [V]')
-    plt.title('Intensity vs Polarizer Angle')
+    plt.xlabel(DEG_LABEL, size=AXIS_LABEL_SIZE)
+    plt.ylabel(INTENSITY_LABEL, size=AXIS_LABEL_SIZE)
+    plt.title('Intensity vs Polarizer Angle', size=GRAPH_TITLE_SIZE)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     if save:
-        plt.savefig("double polarizers.pdf", format="pdf")
+        plt.savefig(f"figures{os.sep}double polarizers.pdf", format="pdf")
     plt.show()
 
 
@@ -96,7 +100,6 @@ def plot_triple_polarizers(angle_polarizer_list, averages_list, save=False):
     I0 = intensity_avarage(I0_file) * 4  # Maximum intensity measured at 45 degrees which is a quarter of the total intensity
     x_values = np.linspace(0, 100, 100)
     I_values = I0 * (np.cos(np.radians(x_values)) * np.sin(np.radians(x_values))) ** 2
-    print(I_values)
     plt.figure(figsize=FIGURE_SIZE)
     plt.errorbar(
         angle_polarizer_list,
@@ -113,23 +116,22 @@ def plot_triple_polarizers(angle_polarizer_list, averages_list, save=False):
         ms=DATA_POINTs_SIZE
     )
     plt.plot(x_values, I_values, color=FIT_COLOR, label=rf'$I = {I0:.2e} \cos^2(\theta)\sin^2(\theta)$')
-    plt.xlabel(r'Angle [$^\circ$]')
-    plt.ylabel('Intensity [V]')
-    plt.title('Intensity vs Polarizer Angle')
+    plt.xlabel(DEG_LABEL, size=AXIS_LABEL_SIZE)
+    plt.ylabel(INTENSITY_LABEL, size=AXIS_LABEL_SIZE)
+    plt.title('Intensity vs Polarizer Angle', size=GRAPH_TITLE_SIZE)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     if save:
-        plt.savefig("triple polarizers.pdf", format="pdf")
+        plt.savefig(f"figures{os.sep}triple polarizers.pdf", format="pdf")
     plt.show()
 
 
-double_polarizers_angles = np.array([0, 10, 350, 20, 340, 80, 260, 170, 180, 160, 270, 250, 100, 200, 300, 70, 90, 355, 330, 150, 190])
+double_polarizers_angles = np.array([0, 10, 350, 20, 340, 80, 270, 250, 100, 300, 70, 90, 355, 330])
 double_polarizers_angles = fix_angles(double_polarizers_angles, 350, 180)
 
-triple_polarizers_angles = np.array(([120, 125, 130, 140, 115, 100, 165, 170, 180, 160, 150, 140, 135]))
+triple_polarizers_angles = np.array(([120, 125, 130, 140, 115, 100, 165, 170, 180, 160, 150, 135]))
 triple_polarizers_angles = fix_angles(triple_polarizers_angles, 75, 90)  # Max value is 120 degrees so zero is 120 - 45
 if __name__ == "__main__":
-    plot_double_polarizers(double_polarizers_angles, extract_averages_from_excel("double polarizers"))
-
-    plot_triple_polarizers(triple_polarizers_angles, extract_averages_from_excel("triple polarizers"))
+    plot_double_polarizers(double_polarizers_angles, extract_averages_from_excel("double polarizers"), True)
+    plot_triple_polarizers(triple_polarizers_angles, extract_averages_from_excel("triple polarizers"), True)
