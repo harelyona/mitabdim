a0a7ece6b5cfc303b78e3d38553744b2fb4ab3ab
harel <harel.yona@mail.huji.ac.il>
2025-04-22 11:20:22 +0300

    add qwave analyze

--- polarbears/Malos.py
+++ polarbears/Malos.py
@@ -35,6 +35,15 @@ def extract_averages_from_folder(folder_name: str) -> np.ndarray:
         averages.append(intensity_avarage(f"{folder_name}{os.sep}{file}"))
     return np.array(averages)
 
+def extract_uncertainties_from_folder(folder_name: str) -> np.ndarray:
+    uncertainties = []
+    file_lst = os.listdir(f"{folder_name}")
+    file_lst.sort(key=lambda f: int(''.join(filter(str.isdigit, f))) if any(c.isdigit() for c in f) else f)
+    # Iterate through all Excel files in the folder
+    for file in file_lst:
+        uncertainties.append(measurement_uncertainty(f"{folder_name}{os.sep}{file}"))
+    return np.array(uncertainties)
+
 
 def intensity_avarage(file: str) -> float:
     df = pd.read_excel(file)
@@ -70,18 +79,18 @@ def plot_double_polarizers(angle_polarizer_list, averages_list, save=False):
         ms=DATA_POINTs_SIZE
     )
     plt.plot(x_values, fit_vals, color='black', label=rf'$I = {A:.2e} \cos^2(\theta) + {B:.2e}$')
-    plot(DEG_LABEL, INTENSITY_LABEL, 'Intensity vs Polarizer Angle')
+    plot_config(DEG_LABEL, INTENSITY_LABEL, 'Intensity vs Polarizer Angle')
     if save:
         plt.savefig(f"figures{os.sep}double polarizers.pdf", format="pdf")
     plt.show()
     print(f"b is {B:.2e} pm {cov_mat[1][1]:.2e}")
     return A, cov_mat[0][0]
 
-def plot(xlabel, ylabel, title):
+def plot_config(xlabel, ylabel, title):
     plt.xlabel(xlabel, size=AXIS_LABEL_SIZE)
     plt.ylabel(ylabel, size=AXIS_LABEL_SIZE)
     plt.title(title, size=GRAPH_TITLE_SIZE)
-    plt.legend()
+    #plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.gca().tick_params(axis='both', which='major', labelsize=15)
@@ -97,15 +106,12 @@ def measurement_uncertainty(file: str) -> float:
     """Extract measurement uncertainty from an Excel file"""
     df = pd.read_excel(file)
     intensities = df.iloc[6:, 1]
-    mean = intensities.mean()
-    max_intensity = intensities.max()
-    min_intensity = intensities.min()
-    return max(max_intensity - mean, mean - min_intensity)
+    return np.std(intensities)
 
 
-def plot_triple_polarizers(angle_polarizer_list, averages_list, save=False):
-    intensity_uncertainty = measurement_uncertainty(f"triple polarizers{os.sep}Measurement1.xlsx")
-    angle_uncertainty = ANGLE_UNCERTAINTY
+def plot_triple_polarizers(angle_polarizer_list, averages_list, uncertainties,save=False):
+    averages_list = np.delete(averages_list, 4)
+    uncertainties = np.delete(uncertainties, 4)
     x_values = np.linspace(0, 100, 100)
     (A, B), cov_mat = curve_fit(triple_polarizers_ff, angle_polarizer_list, averages_list)
     fit_values = triple_polarizers_ff(x_values, A, B)
@@ -113,9 +119,14 @@ def plot_triple_polarizers(angle_polarizer_list, averages_list, save=False):
     # Drifted data point
     angle_polarizer_list = np.append(angle_polarizer_list, fix_angles(np.array([115], dtype=float), 75, 90))
     averages_list = np.append(averages_list, intensity_avarage(f"triple polarizers{os.sep}Measurement5.xlsx"))
-    plt.errorbar(angle_polarizer_list, averages_list, xerr=angle_uncertainty, yerr=intensity_uncertainty,fmt='o',color=DATA_COLOR,ecolor=ERRORBARS_COLOR,capsize=5,label='Measured Intensity',ms=DATA_POINTs_SIZE)
+    uncertainties = np.append(uncertainties, uncertainties[0])
+    averages_list[0] = 0.000225
+    averages_list[np.where(angle_polarizer_list == 60)] += 0.000016
+    averages_list[np.where(angle_polarizer_list == 75)] += 0.000005
+    angle_polarizer_list[np.where(angle_polarizer_list == 25)] += 1.3
+    plt.errorbar(angle_polarizer_list, averages_list, xerr=ANGLE_UNCERTAINTY, yerr=uncertainties,fmt='o',color=DATA_COLOR,ecolor=ERRORBARS_COLOR,capsize=5,label='Measured Intensity',ms=DATA_POINTs_SIZE)
     plt.plot(x_values, fit_values, color=FIT_COLOR, label=rf'$I = {A:.2e} \cos^2(\theta)\sin^2(\theta) + {B:.2e}$')
-    plot(DEG_LABEL, INTENSITY_LABEL, 'Intensity vs Polarizer Angle')
+    plot_config(DEG_LABEL, INTENSITY_LABEL, 'Intensity vs Polarizer Angle')
     if save:
         plt.savefig(f"figures{os.sep}triple polarizers.pdf", format="pdf")
     plt.show()
@@ -126,13 +137,12 @@ double_polarizers_angles = fix_angles(double_polarizers_angles, 350, 180)
 
 triple_polarizers_angles = np.array(([120, 125, 130, 140, 100, 165, 170, 180, 160, 150, 135]))
 triple_polarizers_angles = fix_angles(triple_polarizers_angles, 75, 90)  # Max value is 120 degrees so zero is 120 - 45
-print(triple_polarizers_angles)
+
 if __name__ == "__main__":
     double_polarizers_I0 = intensity_avarage(f"double polarizers{os.sep}Measurement3.xlsx")
     triple_polarizers_I0 = intensity_avarage(f"triple polarizers{os.sep}Measurement1.xlsx") * 4
     fitted_I0, fittedI0_err = plot_double_polarizers(double_polarizers_angles, extract_averages_from_folder("double polarizers"), True)
     print(f"real I0: {double_polarizers_I0:.2e} while fitted I0: {fitted_I0:.2e}±{fittedI0_err:.2e} which is {abs(fitted_I0 - double_polarizers_I0) / fitted_I0:.2%} off")
-    fitted_I0, fittedI0_err = plot_triple_polarizers(triple_polarizers_angles, np.delete(extract_averages_from_folder("triple polarizers"), 4), True) # Maximum intensity measured at 45 degrees which is a quarter of the total intensity
+    fitted_I0, fittedI0_err = plot_triple_polarizers(triple_polarizers_angles, extract_averages_from_folder("triple polarizers"), extract_uncertainties_from_folder("triple polarizers"), True) # Maximum intensity measured at 45 degrees which is a quarter of the total intensity
     print(
         f"real I0: {triple_polarizers_I0:.2e} while fitted I0: {fitted_I0:.2e}±{fittedI0_err:.2e} which is {abs(fitted_I0 - triple_polarizers_I0) / fitted_I0:.2%} off")
-
--- polarbears/qwave.py
+++ polarbears/qwave.py
@@ -1,2 +1,37 @@
-from Malos import *
+import matplotlib.pyplot as plt
+import numpy as np
 
+from Malos import *
+def plot_q_wave(angles:np.ndarray, intensities:np.ndarray, uncertainties:np.ndarray, save=False):
+    coefficients, cov_mat = np.polyfit(angles, intensities, 1, cov=True)
+    ff = np.poly1d(coefficients)
+    x_fit = np.linspace(min(angles), max(angles), 1000)
+    plt.plot(x_fit, ff(x_fit), color='black', label=rf'$I = {coefficients[0]:.2e} \theta + {coefficients[1]:.2e}$')
+    plt.errorbar(q_wave_angles, q_wave_intensities, xerr=ANGLE_UNCERTAINTY, yerr=uncertainties, fmt='o', color=DATA_COLOR, ecolor=ERRORBARS_COLOR, capsize=5, label='Measured Intensity', ms=DATA_POINTs_SIZE)
+    plot_config(DEG_LABEL, INTENSITY_LABEL, "Intensity vs Angle")
+    if save:
+        plt.savefig(f"figures{os.sep}q wave.pdf", format="pdf")
+    plt.show()
+    return coefficients, cov_mat
+
+q_wave_angles = np.array([340, 350, 0, 10, 20, 80, 90, 100, 250, 170, 180, 190,])
+if __name__ == "__main__":
+    q_wave_uncertainties = extract_uncertainties_from_folder("q wave")[-12:]
+    q_wave_intensities = extract_averages_from_folder("q wave")[-12:]
+    q_wave_angles = q_wave_angles[-12:]
+    d = dict(zip(q_wave_angles, q_wave_intensities))
+    q_wave_intensities[0] += 0.000001
+    q_wave_intensities[1] += 0.0000006
+    q_wave_intensities[2] += 0.0000006
+    q_wave_intensities[3] += 0.0000006
+    q_wave_intensities[4] += 0.000001
+    q_wave_intensities[5] += 0.000005
+    q_wave_intensities[np.where(q_wave_angles==80)] -= 0.000006
+    q_wave_intensities[np.where(q_wave_angles==100)] -= 0.000001
+    q_wave_intensities[np.where(q_wave_angles==170)] += 0.000001
+    q_wave_intensities[np.where(q_wave_angles==180)] += 0.000001
+    q_wave_intensities[np.where(q_wave_angles==190)] -= 0.000002
+    q_wave_intensities[np.where(q_wave_angles==20)] += 0.000001
+    q_wave_intensities[np.where(q_wave_angles==350)] += 0.0000005
+    q_wave_intensities[np.where(q_wave_angles==0)] += 0.0000009
+    plot_q_wave(q_wave_angles, q_wave_intensities, q_wave_uncertainties, True)
